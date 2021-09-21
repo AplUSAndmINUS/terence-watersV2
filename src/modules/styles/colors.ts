6cfd0518925050553153993cca53b4d6a1054051
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2021-09-21 15:40:12 -0600

    added color constants

--- src/modules/styles/colors.ts
+++ src/modules/styles/colors.ts
@@ -1,4 +1,46 @@
+export const colorObj = {
+  black: '#000',
+  white: '#FFF',
+  accentColorLight: '#445F7E',
+  accentColorDark: '#5B799B'
+}
+
+export const darkColor = {
+  text: '#FFF',
+  accentColor: colorObj.accentColorDark,
+  largeHeading: colorObj.accentColorDark,
+  pageHeading: colorObj.accentColorDark,
+  subHeading: '#FFF',
+  background: 'radial-gradient(rgba(83,83,83,1) 0%, rgba(56,56,56,1) 64.82%, rgba(92,92,92,1) 100%)',
+  mediaBackground: '#5C5C5C',
+  mediaTextBackground: '#748BA5',
+  mediaCardHeading: '#FFF',
+  mediaCardSubheading: '#E3E3E3',
+  mediaFilterButton: '#5B799B',
+  buttonPrimary: '#5B799B',
+  buttonSecondary: '#5B799B',
+  textFieldBackground: '#748BA5',
+  textFieldText: '#FFF',
+  menuText: colorObj.accentColorDark,
+  menuIcon: colorObj.accentColorDark
+}
+
 export const lightColor = {
-  text: '#ffffff',
-  
+  text: '#383838',
+  accentColor: colorObj.accentColorLight,
+  largeHeading: colorObj.accentColorLight,
+  pageHeading: colorObj.accentColorLight,
+  subHeading: '#383838',
+  background: 'radial-gradient(rgba(255,255,255,1) 0%, rgba(244,244,244,1) 65.52%, rgba(178,177,177,0.22) 100%)',
+  mediaBackground: '#FFF',
+  mediaTextBackground: '#FFF',
+  mediaCardHeading: '#445F7E',
+  mediaCardSubheading: '#748BA5',
+  mediaFilterButton: '#445F7E',
+  buttonPrimary: '#445F7E',
+  buttonSecondary: '#445F7E',
+  textFieldBackground: '#748BA5',
+  textFieldText: '#FFF',
+  menuText: colorObj.accentColorLight,
+  menuIcon: colorObj.accentColorLight
 }
